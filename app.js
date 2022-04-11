eaad16fabe71873ce9222639ded1c853040211ac
waqas96cs <waqaszafar.cs@gmail.com>
2022-04-11 10:41:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// make a repo public on github
+// git init
+// git add .
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/waqas96cs/temp.git
+// git push -u origin main
